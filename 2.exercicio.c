36d53803d4d22ef5f2d888f799780690087f3477
Renxtt <122226822+Renxtt@users.noreply.github.com>
2023-02-19 17:15:23 -0300

    Update 2.exercicio.c

--- 2.exercicio.c
+++ 2.exercicio.c
@@ -5,6 +5,8 @@
 #include <math.h>
 #include <time.h>
 
+// Faça um programa em C que solicite a idade do usuário e verifique se ele é maior ou menor de idade, exibindo a mensagem correspondente na tela.
+
 main(void){
 	setlocale(LC_ALL,"portuguese");
 	int idade, falta;
