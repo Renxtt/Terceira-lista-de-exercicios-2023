16bfebb0c4ac41c5e40bcb3929b7b97bf2f7ab11
Renxtt <122226822+Renxtt@users.noreply.github.com>
2023-02-19 17:11:37 -0300

    Add files via upload

---
+++ 000.exerciciosdaterceiralista.c
@@ -0,0 +1,18 @@
+/*
+
+	# Exercícios 3 - condicionais
+	
+	**Modelo de Programação em C**
+	
+	1. Escreva um programa em C que verifique se um número inteiro é positivo, negativo ou zero, e imprima a respectiva mensagem na tela.
+	2. Faça um programa em C que solicite a idade do usuário e verifique se ele é maior ou menor de idade, exibindo a mensagem correspondente na tela.
+	3. Escreva um programa em C que solicite dois números inteiros e verifique qual é o maior, exibindo o resultado na tela.
+	4. Crie um programa em C que solicite uma letra e verifique se ela é uma vogal ou uma consoante, exibindo a mensagem correspondente na tela.
+	5. Faça um programa em C que solicite o peso e a altura de uma pessoa, e calcule o seu índice de massa corporal (IMC), exibindo a mensagem correspondente na tela (abaixo do peso, peso normal, sobrepeso, obesidade grau 1, obesidade grau 2 ou obesidade grau 3).
+	6. Escreva um programa em C que solicite um número inteiro e verifique se ele é par ou ímpar, exibindo a mensagem correspondente na tela.
+	7. Crie um programa em C que solicite o nome do usuário e verifique se ele é igual a "João" ou "Maria", exibindo a mensagem correspondente na tela.
+	8. Faça um programa em C que solicite o nome e a idade do usuário, e verifique se ele é maior de idade e se o seu nome é igual a "João" ou "Maria", exibindo a mensagem correspondente na tela.
+	9. Escreva um programa em C que solicite três números inteiros e verifique qual é o maior e qual é o menor, exibindo os resultados na tela.
+	10. Crie um programa em C que solicite a idade e a renda mensal de uma pessoa, e verifique se ela é elegível para receber um benefício, considerando as seguintes regras: a pessoa deve ter mais de 18 anos e uma renda mensal de até R$ 1.500,00. Se ela atender a esses requisitos, exiba a mensagem "Parabéns, você é elegível para receber o benefício!". Caso contrário, exiba a mensagem "Infelizmente, você não é elegível para receber o benefício".
+	
+	*/
