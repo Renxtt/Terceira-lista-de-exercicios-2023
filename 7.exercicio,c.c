8446be82429c01016e2f19e3a0775e3268a7661b
Renxtt <122226822+Renxtt@users.noreply.github.com>
2023-02-19 17:13:21 -0300

    Rename 7.exercicio,c.cpp to 7.exercicio,c.c

--- 7.exercicio,c.c
+++ 7.exercicio,c.c
@@ -9,19 +9,19 @@ main(void){
 	setlocale(LC_ALL, "portuguese");
 	
 	char nome[40];
-	//solicite o nome do usu·rio
+	//solicite o nome do usu√°rio
 	printf("\nDigite um Nome: ");
 	scanf("%s", &nome);
 	
-	//verifique se ele È igual a "Jo„o" ou "Maria"
-	if(strcmp(nome,"Jo„o")==0){
-		printf("O nome digitado È igual ‡ Jo„o");
+	//verifique se ele √© igual a "Jo√£o" ou "Maria"
+	if(strcmp(nome,"Jo√£o")==0){
+		printf("O nome digitado √© igual √† Jo√£o");
 		//TODO
 	}else if(strcmp(nome,"Maria")==0){
-		printf("O nome digitado È igual ‡ Maria");
+		printf("O nome digitado √© igual √† Maria");
 		//TODO
-	}else if(strcmp(nome,"Jo„o")==1||strcmp(nome,"Maria")==1){
-		printf("Seu Nome n„o È Jo„o nem Maria");
+	}else if(strcmp(nome,"Jo√£o")==1||strcmp(nome,"Maria")==1){
+		printf("Seu Nome n√£o √© Jo√£o nem Maria");
 		//TODO
 	}
 	return 0;
