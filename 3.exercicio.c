11a6d6ead3d0f0fa908b72821c52a199d92b0245
Renxtt <122226822+Renxtt@users.noreply.github.com>
2023-02-19 17:12:52 -0300

    Rename 3.exercicio.cpp to 3.exercicio.c

--- 3.exercicio.c
+++ 3.exercicio.c
@@ -9,45 +9,45 @@ main(void){
 	setlocale(LC_ALL,"portuguese");
 	int nu, nu2, dif, menor,maior, random, random2;
 	char perg[40], reset[40];
-	printf("VocÍ gostaria de rodar aleatoriamente os n˙meros ou gostaria de escreve-los,(responda com escrever ou rodar)");
-	printf("\ncomo vocÍ gostaria?: ");	
+	printf("Voc√™ gostaria de rodar aleatoriamente os n√∫meros ou gostaria de escreve-los,(responda com escrever ou rodar)");
+	printf("\ncomo voc√™ gostaria?: ");	
 	scanf("%s", &perg);
 	
 	if(strcmp(perg,"escrever")==0||strcmp(perg,"Escrever")==0){
-		printf("Digiti um n˙mero: ");
+		printf("Digiti um n√∫mero: ");
 		scanf("%d", &nu);
-		printf("Digiti um segundo n˙mero: ");
+		printf("Digiti um segundo n√∫mero: ");
 		scanf("%d", &nu2);
 		if(nu>nu2){
 			//TODO
-			printf("%d È maior que %d", nu, nu2);
+			printf("%d √© maior que %d", nu, nu2);
 			
 		}else if(nu2>nu){
-			printf("%d È maior que %d", nu2, nu);
+			printf("%d √© maior que %d", nu2, nu);
 			
 			//TODO
 		}else if(nu==nu2){
-			printf("Ambos s„o iguais!!");
+			printf("Ambos s√£o iguais!!");
 			//TODO
 		}
 		printf("\nGostaria de reiniciar?:");
 		scanf("%s", &reset);				
 	}
 	else if(strcmp(perg,"Rodar")==0||strcmp(perg,"rodar")==0){
-		printf("Digiti o maior n˙mero possivel do seu randomico: ");
+		printf("Digiti o maior n√∫mero possivel do seu randomico: ");
 		scanf("%d", &maior);
 		
 		srand(time(NULL));
 		random=rand() %maior+1;
 		random2=rand() %maior+1;
 		if(random>random2){
-			printf("%d È maior que %d", random, random2);
+			printf("%d √© maior que %d", random, random2);
 			//TODO
 		}else if(random2>random){
-			printf("%d È maior que %d", random2, random);
+			printf("%d √© maior que %d", random2, random);
 			//TODO
 		}else if(random==random2){
-			printf("Ambos s„o iguais!!");
+			printf("Ambos s√£o iguais!!");
 			//TODO
 		}
 		printf("\nGostaria de reiniciar?:");
@@ -55,45 +55,45 @@ main(void){
 		
 		//TODO
 	}while(strcmp(reset,"Sim")==0||strcmp(reset,"sim")==0||strcmp(reset,"s")==0||strcmp(reset,"S")==0){
-		printf("VocÍ gostaria de rodar aleatoriamente os n˙meros ou gostaria de escreve-los,(responda com escrever ou rodar)");
-		printf("\ncomo vocÍ gostaria?: ");	
+		printf("Voc√™ gostaria de rodar aleatoriamente os n√∫meros ou gostaria de escreve-los,(responda com escrever ou rodar)");
+		printf("\ncomo voc√™ gostaria?: ");	
 		scanf("%s", &perg);
 			
 		if(strcmp(perg,"escrever")==0||strcmp(perg,"Escrever")==0){
-			printf("Digiti um n˙mero: ");
+			printf("Digiti um n√∫mero: ");
 			scanf("%d", &nu);
-			printf("Digiti um segundo n˙mero: ");
+			printf("Digiti um segundo n√∫mero: ");
 			scanf("%d", &nu2);
 			if(nu>nu2){
 				//TODO
-				printf("%d È maior que %d", nu, nu2);
+				printf("%d √© maior que %d", nu, nu2);
 				
 			}else if(nu2>nu){
-				printf("%d È maior que %d", nu2, nu);
+				printf("%d √© maior que %d", nu2, nu);
 				
 				//TODO
 			}else if(nu==nu2){
-				printf("Ambos s„o iguais!!");
+				printf("Ambos s√£o iguais!!");
 				//TODO
 			}
 			printf("\nGostaria de reiniciar?:");
 			scanf("%s", &reset);				
 		}
 		else if(strcmp(perg,"Rodar")==0||strcmp(perg,"rodar")==0){
-			printf("Digiti o maior n˙mero possivel do seu randomico: ");
+			printf("Digiti o maior n√∫mero possivel do seu randomico: ");
 			scanf("%d", &maior);
 			
 			srand(time(NULL));
 			random=rand() %maior+1;
 			random2=rand() %maior+1;
 			if(random>random2){
-				printf("%d È maior que %d", random, random2);
+				printf("%d √© maior que %d", random, random2);
 				//TODO
 			}else if(random2>random){
-				printf("%d È maior que %d", random2, random);
+				printf("%d √© maior que %d", random2, random);
 				//TODO
 			}else if(random==random2){
-				printf("Ambos s„o iguais!!");
+				printf("Ambos s√£o iguais!!");
 				//TODO
 			}
 			printf("\nGostaria de reiniciar?:");
